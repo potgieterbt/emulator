15e6efc35cbc96d18fdfeef499eebc2cf4f5db7b
Bennett <potgieterbt@gmail.com>
2023-08-02 13:43:54 +0100

    SDL stuff

--- chip8/sdltest/test.cpp
+++ chip8/sdltest/test.cpp
@@ -1,14 +1,80 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
 
-const int SCREEN_WIDTH = 64;
-const int SCREEN_HEIGHT = 64;
+const int SCREEN_WIDTH = 500;
+const int SCREEN_HEIGHT = 500;
+
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window *gWindow = NULL;
+SDL_Surface *gScreenSurface = NULL;
+SDL_Surface *gHelloWorld = NULL;
 
 int main(int argc, char *argv[]) {
-  SDL_Window *window = NULL;
-  SDL_Surface *screenSurface = NULL;
+  bool quit = false;
+  SDL_Event e;
+
+  if (!init()) {
+    printf("Failed to initialize!\n");
+  } else {
+    if (!loadMedia()) {
+      printf("Failed to load media!\n");
+    } else {
+      SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+      SDL_UpdateWindowSurface(gWindow);
+      SDL_Event e;
+      bool quit = false;
+      while (quit == false) {
+        while (SDL_PollEvent(&e)) {
+          if (e.type == SDL_QUIT)
+            quit = true;
+        }
+      }
+    }
+  }
+  close();
+  return 0;
+}
+
+bool init() {
+  bool success = true;
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    success = false;
+  } else {
+    gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                               SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (gWindow == NULL) {
+      printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+      success = false;
+    } else {
+      gScreenSurface = SDL_GetWindowSurface(gWindow);
+    }
   }
-  return 0;
+
+  return success;
+}
+
+bool loadMedia() {
+  bool success = true;
+  gHelloWorld = SDL_LoadBMP("hello_world.bmp");
+  if (gHelloWorld == NULL) {
+    printf("Unable to load image %s! SDL Error: %s\n", "hello_world.bmp",
+           SDL_GetError());
+    success = false;
+  }
+
+  return success;
+}
+
+void close() {
+  SDL_FreeSurface(gHelloWorld);
+  gHelloWorld = NULL;
+  SDL_DestroyWindow(gWindow);
+  gWindow = NULL;
+  SDL_Quit();
 }

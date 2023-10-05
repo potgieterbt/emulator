7aaf0afa1e4a21766fa66b24b7c84c11faa1b688
Bennett <potgieterbt@gmail.com>
2023-10-05 17:54:02 +0100

    added cartridge

---
+++ NES/cartridge.cpp
@@ -0,0 +1,4 @@
+#include "cartridge.h"
+
+void Cart::init(){
+}
--- NES/cartridge.h
+++ NES/cartridge.h
@@ -1,12 +1,21 @@
 #include <cstdint>
 #include <vector>
 
+enum Mirroring {
+    VERTICAL,
+    HORIZONTAL,
+    FOUR_SCREEN,
+};
+
 class Cart {
 public:
+  void init();
   Cart();
   ~Cart();
   const std::vector<uint8_t> &getROM();
   const std::vector<uint8_t> &getVROM();
+  uint8_t mapper;
+  Mirroring screen_mirroring;
 
 private:
   std::vector<uint8_t> PRG_ROM;

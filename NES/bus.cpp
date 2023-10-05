a31c6a32cdb1fc000cef57439db2b0c02add29ba
Bennett <potgieterbt@gmail.com>
2023-10-05 10:50:13 +0100

    sync

---
+++ NES/bus.cpp
@@ -0,0 +1,43 @@
+#include "bus.h"
+#include <cstdint>
+#include <cstring>
+
+const uint16_t RAM = 0x0000;
+const uint16_t RAM_MIRRORS_END = 0x1FFF;
+const uint16_t PPU_REGISTERS = 0x2000;
+const uint16_t PPU_REGISTERS_MIRRORS_END = 0x3FFF;
+
+void bus::init() { memset(cpu_ram, 0, sizeof(cpu_ram)); }
+
+uint8_t bus::Read(const uint16_t addr) {
+  switch (addr) {
+  case RAM ... RAM_MIRRORS_END: {
+    uint16_t mirror_down_addr = addr & 0b0000011111111111;
+    return cpu_ram[mirror_down_addr];
+  }
+
+  case PPU_REGISTERS ... PPU_REGISTERS_MIRRORS_END: {
+    uint16_t mirror_down_addr = addr & 0b0010000000000111;
+    return 0;
+  }
+  default:
+    return 0;
+  }
+}
+
+void bus::Write(const uint16_t addr, const uint8_t val) {
+  switch (addr) {
+  case RAM ... RAM_MIRRORS_END: {
+    uint16_t mirror_down_addr = addr & 0b11111111111;
+    cpu_ram[mirror_down_addr] = val;
+    return;
+  }
+
+  case PPU_REGISTERS ... PPU_REGISTERS_MIRRORS_END: {
+    uint16_t mirror_down_addr = addr & 0b0010000000000111;
+    return;
+  }
+  default:
+    return;
+  }
+}
---
+++ NES/bus.h
@@ -0,0 +1,13 @@
+#include <cstdint>
+
+class bus {
+  public:
+    uint8_t Read(const uint16_t);
+    void Write(const uint16_t, const uint8_t);
+    uint16_t Read_16(const uint16_t);
+    void Write_16(const uint16_t, const uint16_t);
+
+  private:
+    void init();
+    uint8_t cpu_ram[2048];
+};

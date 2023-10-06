9dbbf5b5d2dd7b1a12744331a9635d082f353e33
Bennett <potgieterbt@gmail.com>
2023-10-06 11:01:14 +0100

    sync

--- NES/cartridge.cpp
+++ NES/cartridge.cpp
@@ -1,4 +1,14 @@
 #include "cartridge.h"
+#include <cstdint>
+#include <vector>
 
 void Cart::init(){
+}
+
+const std::vector<uint8_t>& Cart::getROM(){
+  return PRG_ROM;
+}
+
+const std::vector<uint8_t>& Cart::getVROM(){
+  return CHR_ROM;
 }
--- NES/cartridge.h
+++ NES/cartridge.h
@@ -1,23 +1,24 @@
 #include <cstdint>
+#include <string>
 #include <vector>
-
-enum Mirroring {
-    VERTICAL,
-    HORIZONTAL,
-    FOUR_SCREEN,
-};
 
 class Cart {
 public:
   void init();
   Cart();
   ~Cart();
+  bool loadFromFile(std::string path);
   const std::vector<uint8_t> &getROM();
   const std::vector<uint8_t> &getVROM();
-  uint8_t mapper;
-  Mirroring screen_mirroring;
+  uint8_t getMapper();
+  uint8_t getNameTableMirroring();
+  bool hasExtendedRAM();
 
 private:
   std::vector<uint8_t> PRG_ROM;
   std::vector<uint8_t> CHR_ROM;
+  uint8_t nameTableMirroring;
+  uint8_t mapperNumber;
+  bool extendedRAM;
+  bool chrRAM;
 };

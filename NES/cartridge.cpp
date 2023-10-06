44b3331e4b745989cc903376ed1ea2c7c0662b69
Bennett <potgieterbt@gmail.com>
2023-10-06 12:00:15 +0100

    sync

--- NES/cartridge.cpp
+++ NES/cartridge.cpp
@@ -1,14 +1,35 @@
 #include "cartridge.h"
 #include <cstdint>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <string>
 #include <vector>
 
-void Cart::init(){
-}
+const std::vector<uint8_t> &Cart::getROM() { return PRG_ROM; }
 
-const std::vector<uint8_t>& Cart::getROM(){
-  return PRG_ROM;
-}
+const std::vector<uint8_t> &Cart::getVROM() { return CHR_ROM; }
 
-const std::vector<uint8_t>& Cart::getVROM(){
-  return CHR_ROM;
+uint8_t Cart::getMapper() { return mapperNumber; }
+
+uint8_t Cart::getNameTableMirroring() { return nameTableMirroring; }
+
+bool Cart::hasExtendedRAM() { return true; }
+
+bool Cart::loadFromFile(std::string path) {
+  std::ifstream romFile(path, std::ios_base::binary | std::ios_base::in);
+  if (!romFile) {
+    std::cout << "Could not open ROM file from path: " << path << "\n";
+    return false;
+  }
+
+  std::vector<uint8_t> header;
+  std::cout << "Reading ROM from path: " << path << "\n";
+
+  header.resize(0x10);
+  if (!romFile.read(reinterpret_cast<char*>(&header[0]), 0x10)) {
+
+  }
+
+  return true;
 }

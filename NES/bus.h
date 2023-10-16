13aabd07b2ee3e53fd0dbe949428cceb10a9257d
Bennett <potgieterbt@gmail.com>
2023-10-16 12:01:37 +0100

    sync

--- NES/bus.h
+++ NES/bus.h
@@ -1,13 +1,16 @@
 #include <cstdint>
 
-class bus {
-  public:
-    uint8_t Read(const uint16_t);
-    void Write(const uint16_t, const uint8_t);
-    uint16_t Read_16(const uint16_t);
-    void Write_16(const uint16_t, const uint16_t);
+namespace nesem {
 
-  private:
-    void init();
-    uint8_t cpu_ram[2048];
+class bus {
+public:
+  uint8_t Read(const uint16_t);
+  void Write(const uint16_t, const uint8_t);
+  uint16_t Read_16(const uint16_t);
+  void Write_16(const uint16_t, const uint16_t);
+
+private:
+  void init();
+  uint8_t cpu_ram[2048];
 };
+} // namespace nes

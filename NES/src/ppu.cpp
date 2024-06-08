41372bc1c1f3c5c78025f859357f6c501b63306f
Bennett Potgieter <potgieterbt@gmail.com>
2024-06-08 10:27:09 +0100

    More ppu

---
+++ NES/src/ppu.cpp
@@ -0,0 +1,8 @@
+#include "ppu.hpp"
+
+void ppu::tick() {
+    if (scanLine >= 0 & scanLine <= 239 || scanLine == 261) {
+
+    }
+  return;
+}
--- NES/src/ppu.hpp
+++ NES/src/ppu.hpp
@@ -4,12 +4,51 @@
 
 class ppu {
 public:
-  const uint8_t height = 242;
-  const uint16_t width = 283;
+  const int ScanlineCycleLength = 341;
+  const int ScanlineEndCycle = 340;
+  const int VisibleScanlines = 240;
+  const int ScanlineVisibleDots = 256;
+  const int FrameEndScanline = 261;
 
-  uint8_t PPUCTRL;
-  uint8_t PPUMASK;
-  uint8_t PPUSTATUS;
+  bool rendering;
+
+  union {
+    struct {
+      unsigned nametableAddress : 2;
+      unsigned vramIncrement : 1;
+      unsigned spritePatternTable : 1;
+      unsigned bgPatternTable : 1;
+      unsigned spriteSize : 1;
+      unsigned ppuMasterSlaveSelect : 1;
+      unsigned genNMI : 1;
+    };
+    uint8_t val;
+  } PPUCTRL;
+
+  union {
+    struct {
+      unsigned greyscale : 1;
+      unsigned showLeftBackground : 1;
+      unsigned showLeftSprites : 1;
+      unsigned showBackground : 1;
+      unsigned showSprites : 1;
+      unsigned emphasizeRed : 1;
+      unsigned emphasizeGreen : 1;
+      unsigned emphasizeBlue : 1;
+    };
+    uint8_t val;
+  } PPUMASK;
+
+  union {
+    struct {
+      unsigned leastSignificantBits : 5;
+      unsigned spriteOverflow : 1;
+      unsigned spriteZeroHit : 1;
+      unsigned vBlank : 1;
+    };
+    uint8_t val;
+  } PPUSTATUS;
+
   uint8_t OAMADDR;
   uint8_t OAMDATA;
   uint8_t PPUSCROLL;
@@ -21,6 +60,10 @@ public:
   std::array<uint8_t, 32> pallete_table;
   std::array<uint8_t, 2048> vram;
   std::array<uint8_t, 256> oam_data;
+
+  int scanLine = 0;
+  int dot = 0;
+  bool odd = false;
 
   enum Mirroring {
     Horizontal = 0,
